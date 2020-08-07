131352c7258d8802f5b34ac55510e4b32e012266
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 12:03:50 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Anything you want to say";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.3;
+            bool isMale = true;
+
+            Console.ReadLine();
+        }
+    }
+}
